43a227f727371c37095ed98188c3b4a722c88ceb
Алексей <alekselion@mail.ru>
2021-11-18 20:24:03 +0300

    Add files via upload

--- ffnn_black.py
+++ ffnn_black.py
@@ -5,8 +5,43 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+# ###########
+# PRE-SETTING 
+# ###########
 
-# create model
+# color
+textColor = (255, 0, 255)  # text color (purple)
+contentColor = (0, 0, 0)  # canvas color (black)
+fieldsColor = (50, 50, 50)  # header and footer color (gray)
+
+# size
+widthCanvas, heightCanvas = 300, 400  # canvas size
+widthFields, heightFields = widthCanvas, 50  # header and footer size
+widthContent, heightContent = widthCanvas, heightCanvas - heightFields * 2  # content size
+
+# canvas
+canvas = np.zeros((heightCanvas, widthCanvas, 3), np.uint8)  # create
+
+# header
+header = np.zeros((heightFields, widthFields, 3), np.uint8)  # create
+header[:, :, :] = fieldsColor  # change color
+canvas[:heightFields, :, :] = header  # add to canvas
+
+# content
+content = np.zeros((heightContent, widthFields, 3), np.uint8)  # create
+content[:, :, :] = contentColor  # change color
+canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
+
+# footer
+footer = np.zeros((heightFields, widthFields, 3), np.uint8)  # create
+footer[:, :, :] = fieldsColor  # change color
+canvas[heightCanvas-heightFields:, :, :] = footer  # add to canvas
+
+
+# ############
+# CREATE MODEL
+# ############
+
 class Net(nn.Module):
     def __init__(self, hid1, hid2):
         super(Net, self).__init__()
@@ -25,64 +60,75 @@ class Net(nn.Module):
         return x
 
 
-model = Net(100, 80)
-
-# load model
+model = Net(100, 80)  # create model
 pth = os.path.join(os.getcwd(), "models")
-model.load_state_dict(torch.load(pth + "/model_ffnn.pth"))
+model.load_state_dict(torch.load(pth + "/model_ffnn.pth"))  # load model weights
 
-# canvas
-w, h = 300, 300
-canvas = np.zeros((h, w, 3), np.uint8)  # create canvas
-title = np.zeros((30, w, 3), np.uint8)  # create title
-title[:, :, :] = (50, 50, 50)  # change color for title
+# ########
+# RUN TEST
+# ########
+
+# change footer
+cv2.putText(footer, "Left mouse button to draw", (10, 18), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+cv2.putText(footer, "Right mouse button to clear", (10, 42), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+canvas[heightCanvas-heightFields:, :, :] = footer  # add to canvas
+footer[:, :, :] = fieldsColor  # clear footer
+
 flag = False
 
 
-def drawCircle(event, x, y, flags, param):
-    """ left mouse button - draw on canvas
-        right mouse button - clear canvas
+def drawCircle(event, x, y, *otherParams):
+    """ left mouse button to draw
+        right mouse button to clear
     """
     global flag
     if event == cv2.EVENT_LBUTTONDOWN:
         flag = True
-        cv2.circle(canvas, (x,y), 15, (255, 255, 255), -1)
-        title[:, :, :] = (50, 50, 50)
+        cv2.circle(content, (x, y-heightFields), 15, (255, 255, 255), -1)
+        canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
     elif event == cv2.EVENT_LBUTTONUP:
         flag = False
 
     # draw
     if flag and event == cv2.EVENT_MOUSEMOVE:
-        cv2.circle(canvas, (x,y), 15, (255, 255, 255), -1)
-        title[:, :, :] = (50, 50, 50)
+        cv2.circle(content, (x, y-heightFields), 15, (255, 255, 255), -1)
+        canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
 
     # clear
     if not flag and event == cv2.EVENT_RBUTTONDOWN:
-        canvas[:, :, :] = (0, 0, 0)
-        title[:, :, :] = (50, 50, 50)
+        content[:, :, :] = contentColor  # clear content
+        canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
 
 
+emptyContent = content.copy()
 cv2.namedWindow("canvas")  
 cv2.setMouseCallback("canvas", drawCircle)
-while True:
-    # image processing
-    img = canvas[30:, :, :].copy()  # copy canvas without title
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # change image color to gray
-    img = cv2.resize(img, (28, 28))  # image resize to 28*28
-    img = torch.from_numpy(img)  # image to tensor
-    img = img / 255.0  # image normalize
+while True: 
+    img = content.copy()  # copy content
+    if img.any() == emptyContent.any():  # check content is empty
+        # change header
+        cv2.putText(header, "Canvas is empty", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+        canvas[:heightFields, :, :] = header  # add header to canvas
+        header[:, :, :] = fieldsColor  # clear header
+    else:
+        # image processing
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # change image color to gray
+        img = cv2.resize(img, (28, 28))  # image resize to 28*28
+        img = torch.from_numpy(img)  # image to tensor      
+        img = img / 255.0  # image normalize
+
+        # predict
+        model.eval()
+        pred = (model.forward(img))
+        predNumber = int(pred.argmax(dim=1))
+        predAccuracy = float(pred[0][predNumber]) * 100
+        result = f"Number: {predNumber} ({predAccuracy :.2f}%)"
+        
+        # change header
+        cv2.putText(header, result, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+        canvas[:heightFields, :, :] = header  # add header to canvas
+        header[:, :, :] = fieldsColor  # clear header
     
-    # predict
-    model.eval()
-    pred = (model.forward(img))
-    predNumber = int(pred.argmax(dim=1))
-    predAccuracy = float(pred[0][predNumber]) * 100
-    result = f"{predNumber} ({predAccuracy :.2f}%)"
-    
-    # show
-    cv2.putText(title, "Number:", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-    cv2.putText(title, result, (85, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-    canvas[:30, :] = title
     cv2.imshow("canvas", canvas)
     if cv2.waitKey(1) & 0xFF == ord("q"):  
         break
--- main.py
+++ main.py
@@ -5,21 +5,61 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-colorText = (255, 0, 255)
+# ###########
+# PRE-SETTING 
+# ###########
+
+# color
+textColor = (255, 0, 255)  # text color (purple)
+contentColor = (0, 0, 0)  # canvas color (black)
+fieldsColor = (50, 50, 50)  # header and footer color (gray)
+
+# size
+widthCanvas, heightCanvas = 300, 400  # canvas size
+widthFields, heightFields = widthCanvas, 50  # header and footer size
+widthContent, heightContent = widthCanvas, heightCanvas - heightFields * 2  # content size
+
+# canvas
+canvas = np.zeros((heightCanvas, widthCanvas, 3), np.uint8)  # create
+
+# header
+header = np.zeros((heightFields, widthFields, 3), np.uint8)  # create
+header[:, :, :] = fieldsColor  # change color
+canvas[:heightFields, :, :] = header  # add to canvas
+
+# content
+content = np.zeros((heightContent, widthFields, 3), np.uint8)  # create
+content[:, :, :] = contentColor  # change color
+canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
+
+# footer
+footer = np.zeros((heightFields, widthFields, 3), np.uint8)  # create
+footer[:, :, :] = fieldsColor  # change color
+canvas[heightCanvas-heightFields:, :, :] = footer  # add to canvas
+
+# # show canvas
+# cv2.imshow("Sample canvas", canvas)
+# cv2.waitKey(0)
+# cv2.destroyWindow("Sample canvas")
 
 # ############
 # SELECT MODEL
 # ############
- 
-# create canvas for model selection
-canvas = np.zeros((300, 300, 3), np.uint8)
-canvas[:, 150:, :] = (255, 255, 255)
-cv2.putText(canvas, "Click on model", (60, 30), cv2.FONT_HERSHEY_COMPLEX, 0.7, colorText, 2)
-cv2.putText(canvas, "to select it", (85, 60), cv2.FONT_HERSHEY_COMPLEX, 0.7, colorText, 2)
-cv2.putText(canvas, "Convolutional", (10, 135), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colorText, 2)
-cv2.putText(canvas, "Neural Network", (3, 165), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colorText, 2)
-cv2.putText(canvas, "Feed Forward", (160, 135), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colorText, 2)
-cv2.putText(canvas, "Neural Network", (153, 165), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colorText, 2)
+
+# change header
+cv2.putText(header, "Click on model to select it", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+canvas[:heightFields, :, :] = header  # add header to canvas
+header[:, :, :] = fieldsColor  # clear header
+
+# change content
+content[:, widthContent//2:, :] = (255, 255, 255)  # split content vertically
+cv2.putText(content, "Convolutional", (10, 135), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+cv2.putText(content, "Neural Network", (3, 165), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+cv2.putText(content, "Feed Forward", (160, 135), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+cv2.putText(content, "Neural Network", (153, 165), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add content to canvas
+content[:, :, :] = contentColor  # clear content
+
 modelName = None
 
 
@@ -36,36 +76,6 @@ while True:
     if modelName: break
     cv2.waitKey(1)
 cv2.destroyWindow("Select model")
-
-# ############
-# SELECT COLOR
-# ############
-
-# create canvas for color selection
-canvas = np.zeros((300, 300, 3), np.uint8)
-canvas[:, 150:, :] = (255, 255, 255)
-cv2.putText(canvas, "Click on color", (60, 30), cv2.FONT_HERSHEY_COMPLEX, 0.7, colorText, 2)
-cv2.putText(canvas, "to select it", (85, 60), cv2.FONT_HERSHEY_COMPLEX, 0.7, colorText, 2)
-cv2.putText(canvas, "Black", (45, 135), cv2.FONT_HERSHEY_COMPLEX, 0.7, colorText, 2)
-cv2.putText(canvas, "Canvas", (35, 165), cv2.FONT_HERSHEY_COMPLEX, 0.7, colorText, 2)
-cv2.putText(canvas, "White", (195, 135), cv2.FONT_HERSHEY_COMPLEX, 0.7, colorText, 2)
-cv2.putText(canvas, "Canvas", (185, 165), cv2.FONT_HERSHEY_COMPLEX, 0.7, colorText, 2)
-colorName = None
-
-
-def selectColor(event, x, y, *otherParams):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        global colorName
-        colorName = "black" if x <= 150 else "white"
-
-
-cv2.namedWindow("Select color")  
-cv2.setMouseCallback("Select color", selectColor)
-while True:
-    cv2.imshow("Select color", canvas)
-    if colorName: break
-    cv2.waitKey(1)
-cv2.destroyWindow("Select color")
 
 # ##########
 # LOAD MODEL
@@ -146,81 +156,134 @@ elif modelName == "cnn":
     pth = os.path.join(os.getcwd(), "models")
     model.load_state_dict(torch.load(pth + "/model_cnn.pth"))
 
-# #############
-# CREATE CANVAS
-# #############
+# ############
+# SELECT COLOR
+# ############
 
-w, h = 300, 400
-canvas = np.zeros((h, w, 3), np.uint8)  # create canvas
-header = np.zeros((30, w, 3), np.uint8)  # create header
-footer = np.zeros((60, w, 3), np.uint8)  # create footer
+# change header
+cv2.putText(header, "Click on color to select it", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+canvas[:heightFields, :, :] = header  # add header to canvas
+header[:, :, :] = fieldsColor  # clear header
 
-# initialize color
-canvasColor = (255, 255, 255) if colorName == "white" else (0, 0, 0)  # color for canvas
-headerColor = (205, 205, 205) if colorName == "white" else (50, 50, 50)  # color for header
-footerColor = (205, 205, 205) if colorName == "white" else (50, 50, 50)  # color for footer
+# change content
+content[:, widthContent//2:, :] = (255, 255, 255)  # split content vertically
+cv2.putText(content, "Black canvas", (15, 135), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+cv2.putText(content, "Background", (20, 165), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+cv2.putText(content, "White canvas", (165, 135), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+cv2.putText(content, "Background", (170, 165), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add content to canvas
+content[:, :, :] = contentColor  # clear content
+
+colorName = None
+
+
+def selectColor(event, x, y, *otherParams):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        global colorName
+        colorName = "black" if x <= 150 else "white"
+
+
+cv2.namedWindow("Select color")  
+cv2.setMouseCallback("Select color", selectColor)
+while True:
+    cv2.imshow("Select color", canvas)
+    if colorName: break
+    cv2.waitKey(1)
+cv2.destroyWindow("Select color")
+
+# ##########
+# SET COLORS
+# ##########
+
+contentColor = (255, 255, 255) if colorName == "white" else (0, 0, 0)  # color for content
+fieldsColor = (205, 205, 205) if colorName == "white" else (50, 50, 50)  # color for header and footer
 penColor = (0, 0, 0) if colorName == "white" else (255, 255, 255)  # color for painting
 
 # change color
-canvas[:, :, :] = canvasColor  # color for canvas
-header[:, :, :] = headerColor  # color for header
-footer[:, :, :] = footerColor  # color for footer
-cv2.putText(footer, "Left mouse button to draw", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colorText, 2)
-cv2.putText(footer, "Right mouse button to clear", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colorText, 2)
-canvas[340:, :, :] = footer
+header[:, :, :] = fieldsColor  # color for header
+canvas[:heightFields, :, :] = header  # add to canvas
+content[:, :, :] = contentColor  # color for content
+canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
+footer[:, :, :] = fieldsColor  # color for footer
+canvas[heightCanvas-heightFields:, :, :] = footer  # add to canvas
+
+# # show canvas
+# cv2.imshow("Sample canvas", canvas)
+# cv2.waitKey(0)
+# cv2.destroyWindow("Sample canvas")
+
+# ########
+# RUN TEST
+# ########
+
+# change footer
+cv2.putText(footer, "Left mouse button to draw", (10, 18), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+cv2.putText(footer, "Right mouse button to clear", (10, 42), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+canvas[heightCanvas-heightFields:, :, :] = footer  # add to canvas
+footer[:, :, :] = fieldsColor  # clear footer
+
 flag = False
 
 
-def drawCircle(event, x, y, flags, param):
-    """ left mouse button - draw on canvas
-        right mouse button - clear canvas
+def drawCircle(event, x, y, *otherParams):
+    """ left mouse button to draw
+        right mouse button to clear
     """
     global flag
     if event == cv2.EVENT_LBUTTONDOWN:
         flag = True
-        cv2.circle(canvas, (x,y), 15, penColor, -1)
-        header[:, :, :] = headerColor
+        cv2.circle(content, (x, y-heightFields), 15, penColor, -1)
+        canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
     elif event == cv2.EVENT_LBUTTONUP:
         flag = False
 
     # draw
     if flag and event == cv2.EVENT_MOUSEMOVE:
-        cv2.circle(canvas, (x,y), 15, penColor, -1)
-        header[:, :, :] = headerColor
+        cv2.circle(content, (x, y-heightFields), 15, penColor, -1)
+        canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
 
     # clear
     if not flag and event == cv2.EVENT_RBUTTONDOWN:
-        canvas[:340, :, :] = canvasColor
-        header[:, :, :] = headerColor
+        content[:, :, :] = contentColor  # clear content
+        canvas[heightFields:heightCanvas-heightFields, :, :] = content  # add to canvas
 
 
+emptyContent = content.copy()
 cv2.namedWindow("canvas")  
 cv2.setMouseCallback("canvas", drawCircle)
-while True:
-    # image processing
-    img = canvas[30:340, :, :].copy()  # copy canvas without header and footer
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # change image color to gray
-    img = cv2.resize(img, (28, 28))  # image resize to 28*28
-    img = torch.from_numpy(img)  # image to tensor
-    if colorName == "white":
-        img = 255.0 - img  # image inversion
+while True: 
+    img = content.copy()  # copy content
+    isEmpty = img.all() == emptyContent.all() if colorName == "white" else img.any() == emptyContent.any()
+    if isEmpty:  # check content is empty
+        # change header
+        cv2.putText(header, "Canvas is empty", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+        canvas[:heightFields, :, :] = header  # add header to canvas
+        header[:, :, :] = fieldsColor  # clear header
+    else:
+        # image processing
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # change image color to gray
+        img = cv2.resize(img, (28, 28))  # image resize to 28*28
+        img = torch.from_numpy(img)  # image to tensor
+        if colorName == "white":
+            img = 255.0 - img  # image inversion
+        
+        img = img / 255.0  # image normalize
+        if modelName == "cnn":
+            img = img.unsqueeze(0)  # add 'batch size'  img.shape = (B, W, H)
+            img = img.unsqueeze(1)  # add 'channel'  img.shape = (B, C, W, H)
+        
+        # predict
+        model.eval()
+        pred = (model.forward(img))
+        predNumber = int(pred.argmax(dim=1))
+        predAccuracy = float(pred[0][predNumber]) * 100
+        result = f"Number: {predNumber} ({predAccuracy :.2f}%)"
+        
+        # change header
+        cv2.putText(header, result, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, textColor, 2)
+        canvas[:heightFields, :, :] = header  # add header to canvas
+        header[:, :, :] = fieldsColor  # clear header
     
-    img = img / 255.0  # image normalize
-    if modelName == "cnn":
-        img = img.unsqueeze(0)  # add 'batch size'  img.shape = (B, W, H)
-        img = img.unsqueeze(1)  # add 'channel'  img.shape = (B, C, W, H)
-    
-    # predict
-    model.eval()
-    pred = (model.forward(img))
-    predNumber = int(pred.argmax(dim=1))
-    predAccuracy = float(pred[0][predNumber]) * 100
-    result = f"{predNumber} ({predAccuracy :.2f}%)"
-    
-    # show
-    cv2.putText(header, "Number:", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colorText, 2)
-    cv2.putText(header, result, (95, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colorText, 2)
-    canvas[:30, :, :] = header
     cv2.imshow("canvas", canvas)
     if cv2.waitKey(1) & 0xFF == ord("q"):  
         break
